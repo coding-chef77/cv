3eeccb995ff0c389bd7acbd9c85454da899e570c
Heine Günther <heine_gunther@hotmail.com>
2023-10-05 18:53:01 +0200

    Created objecs to make it more dynamic

---
+++ resumeData.js
@@ -0,0 +1,104 @@
+const cvData = {
+  header: {
+    image: "avatar.png",
+    altText: "Portrait of Heine Günther",
+    name: "Heine Günther",
+    title: "fra kokk til koder",
+    address: {
+      street: "Sponstuveien 8, 1263 Oslo",
+      email: "heine_gunther@hotmail.com",
+      linkedIn: "https://www.linkedin.com/in/heine-günther/",
+      phone: "+47 991 14 767",
+    },
+  },
+  profil: {
+    profileDescription:
+      "Med bakgrunn fra mat- og logistikk og en nylig fullført toårig utdannelse som frontend utvikler...",
+    utdanning: [
+      {
+        title: "Fronten-utvikling, Noroff",
+        startDate: "03.21",
+        endDate: "03.23",
+        description:
+          "I min Utdanning fikk jeg innførinf i relavante bransjeverktøy...",
+      },
+      {
+        title: "Bachelor ernæring, Atlantis MH",
+        startDate: "08.08",
+        endDate: "06.11",
+        description:
+          "Bachelor i ernæring er et studium som gir ernæringsfaglig...",
+      },
+      {
+        title: "Kokkeskole, Karmsund/Godalen VGS",
+        startDate: "08.93",
+        endDate: "06.95",
+        description: "Grunnfag kokk/servitør og VK1 kokkelinje.",
+      },
+    ],
+    kompetanse: {
+      faglig: [
+        "Javascript",
+        "HTML5",
+        "CSS3",
+        "React",
+        "VS Code",
+        "Bootstrap5",
+        "ChatGPT",
+        "Github",
+      ],
+      personlig: [
+        "Strukturert",
+        "Pålitelig",
+        "Lærevillig",
+        "Selvstendig",
+        "Inkluderende",
+        "Imøtekommende",
+        "Ansvarsfull",
+        "Kommunikasjon",
+      ],
+    },
+    omMeg:
+      "På fritiden liker jeg å gjøre aktiviteter sammen med min familie...",
+    referanser: "Oppgis på forespørsel",
+  },
+  erfaring: [
+    {
+      title: "Bjølsenhjemmet, Kokk (Logistikk)",
+      startDate: "09.11",
+      endDate: "12.20",
+      description: "Som ansvarlig for matlogistikk og innkjøp...",
+    },
+    {
+      title: "Folkets Café, Kokk",
+      startDate: "06.11",
+      endDate: "08.11",
+      description: "Som den eneste kokken i oppstarten av Folkets Café...",
+    },
+    {
+      title: "Ekit, Lager/produkttesting",
+      startDate: "06.05",
+      endDate: "01.08",
+      description:
+        "Jobbet med teamet på produkttesting og lagerstyring. Delvis Boston USA og Storbritania.",
+    },
+    {
+      title: "Eidesvik AS, Forpleiningssjef (Stuert)",
+      startDate: "04.99",
+      endDate: "09.05",
+      description: "Min karriere har ledet meg til jobb over hele verden...",
+    },
+    {
+      title: "Restauranter i Stavanger/Haugesund",
+      startDate: "08.97",
+      endDate: "04.99",
+      description: "Kokkeyrket er mangfoldig og stiller høye krav...",
+    },
+    {
+      title: "CCC Catering, Kokkelærling",
+      startDate: "08.95",
+      endDate: "08.97",
+      description: "Som kokkelærling må man gjennom en rekke faser...",
+    },
+  ],
+};
